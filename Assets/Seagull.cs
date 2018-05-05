5f8c33b4c9faec7d24d1d0aa6e3fb22c8a7571e7
Martin Briand <martinbriands@gmail.com>
2018-05-05 15:41:57 -0400

    merging

---
+++ Assets/Seagull.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seagull : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.right * 0.05f;
+
+	}
+
+	//TODO after hooked, dissapear in an explosion of feathers
+}
--- Assets/StageManager.cs
+++ Assets/StageManager.cs
@@ -8,6 +8,9 @@ public class StageManager : MonoBehaviour {
 	public GameState state;
 	public int gameDuration = 180;
 
+	public GameObject seagull;
+	public Transform seagull_origin;
+
 	public GameObject iceberg_small;
 	public Transform iceberg_origin;
 
@@ -29,7 +32,9 @@ public class StageManager : MonoBehaviour {
 		sky_stormy.SetActive (false);
 		while (state == GameState.Boat) {
 			//send seaguls
-			yield return new WaitForSeconds(1f);
+			yield return new WaitForSeconds(Random.Range(5,10));
+			Instantiate (seagull, seagull_origin.position + new Vector3(0, Random.Range(0,15f), 0), seagull_origin.rotation);
+
 		}
 	}
 
@@ -38,7 +43,7 @@ public class StageManager : MonoBehaviour {
 		sky_stormy.SetActive (true);
 		while (state == GameState.Storm) {
 			//send small icebergs
-			yield return new WaitForSeconds(5f);
+			yield return new WaitForSeconds(Random.Range(2,5));
 
 			Instantiate (iceberg_small, iceberg_origin.position + new Vector3(0, Random.Range(-2f,2f), 0), iceberg_origin.rotation);
 		}
