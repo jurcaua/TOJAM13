71d8c30cfb59a85fbe67e5fabf2239722bb27cee
Martin Briand <martinbriands@gmail.com>
2018-05-04 20:10:20 -0400

    fishing line

---
+++ Assets/FishingLine.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingLine : MonoBehaviour {
+
+	public int segmentCount;
+	public Transform fire;
+
+	public float fireStrength;
+	public float segmentScale = 1;
+
+	public LineRenderer sightLine;
+	private Collider2D _hitObject;
+	public Collider2D hitObject { get { return _hitObject; } }
+
+	public LineRenderer straightLine;
+
+	// Use this for initialization
+	void Start () {
+		sightLine = GetComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//SimulatePath ();
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			StartCoroutine (Shoot ());
+		}
+
+	}
+
+	IEnumerator Shoot() {
+
+		int throwing = 0;
+
+		Vector3[] segments = new Vector3[segmentCount];
+		SimulatePath (segments);
+
+		while (throwing < segmentCount + 1) {
+			for (int i = 0; i < throwing; i++) {
+				sightLine.positionCount = throwing;
+				sightLine.SetPosition (i, segments [i]);
+			}
+			throwing++;
+			yield return new WaitForFixedUpdate();
+		}
+	}
+
+	public void SimulatePath(Vector3[] segments) {
+		//Vector3[] segments = new Vector3[segmentCount];
+		segments [0] = fire.position;
+		Vector3 segVelocity = -fire.transform.up * fireStrength * Time.deltaTime;
+		_hitObject = null;
+
+		for (int i = 1; i < segmentCount; i++) {
+			float segTime = (segVelocity.sqrMagnitude != 0) ? segmentScale / segVelocity.magnitude : 0;
+			segVelocity = segVelocity + Physics.gravity * segTime;
+
+			//Bounce shit
+						RaycastHit2D hit;
+			if (hit = Physics2D.Raycast(segments[i-1], segVelocity, segmentScale)) {
+			//			if (Physics2D.Raycast (segments [i - 1], segVelocity, out hit, segmentScale)) {
+			//				_hitObject = hit.collider;
+			//
+							segments [i] = segments [i - 1] + segVelocity.normalized * hit.distance;
+							segVelocity = segVelocity - Physics.gravity * (segmentScale - hit.distance) / segVelocity.magnitude;
+							segVelocity = Vector3.Reflect (segVelocity, hit.normal);
+
+				straightLine.SetPosition (0, fire.position);
+				straightLine.SetPosition (1, segments[i]);
+
+						} else {
+
+			segments [i] = segments [i - 1] + segVelocity * segTime;
+						}
+		}
+
+		//color and transparency
+		Color startColor = Color.white;
+		Color endColor = startColor;
+		startColor.a = 1;
+		endColor.a = 0;
+		sightLine.startColor = startColor;
+		sightLine.endColor = endColor;
+
+		sightLine.positionCount = segmentCount;
+
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+
+	public string str;
+	public float winnerY;
+
+	public int p1Score;
+	public int p2Score;
+
+	public Text p1Text;
+	public Text p2Text;
+
+	public Slider p1Slider;
+	public Slider p2Slider;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player1.transform.position.y == player2.transform.position.y) {
+			//tie
+			str = "tie";
+		} else if (player1.transform.position.y < player2.transform.position.y) {
+			//player2 wins
+			str = "player2";
+			winnerY = player2.transform.position.y;
+
+			p2Slider.value++;
+			if (p2Slider.value == p2Slider.maxValue) {
+				p2Score++;
+				p2Slider.value = 0;
+				p2Text.text = "P2: " + p2Score;
+			}
+			p1Slider.value = 0;
+
+
+		} else {
+			//player1 wins
+			str = "player1";
+			winnerY = player1.transform.position.y;
+			p1Slider.value++;
+			if (p1Slider.value == p1Slider.maxValue) {
+				p1Score++;
+				p1Slider.value = 0;
+				p1Text.text = "P1: " + p1Score;
+			}
+			p2Slider.value = 0;
+
+
+
+		}
+
+		if (str != "tie") {
+			GetComponent<LineRenderer> ().SetPosition (0, new Vector3(-18, winnerY, 0));
+			GetComponent<LineRenderer> ().SetPosition (1, new Vector3(18, winnerY, 0));
+		}
+			
+	}
+}
