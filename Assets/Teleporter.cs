6496e5ebe43921f2257dbec74d798ddbbe14a60c
Alex <jurcaua@gmail.com>
2018-05-06 19:25:46 -0400

    all menus have background

---
+++ Assets/Teleporter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public GameObject waitingFor;
+    public Transform teleportTo;
+
+	void OnTriggerEnter2D(Collider2D c) {
+        if (c.gameObject == waitingFor) {
+            c.transform.position = teleportTo.position;
+        }
+    }
+}
