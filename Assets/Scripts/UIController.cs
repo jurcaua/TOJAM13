ac5dcf0cee7472ad6cd5a8359d45bf56ce2f3b3c
Alex <jurcaua@gmail.com>
2018-05-12 23:57:03 -0400

    players can select their own color now!

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -12,6 +12,7 @@ public class ControlSetting {
     public ControlType controlType;
     public GamePad.Index controllerIndex;
     public bool isSet;
+    public Color selectedColor = Color.white;
 
     public ControlSetting (ControlType _controlType, bool _isSet = true, GamePad.Index _controllerIndex = GamePad.Index.Any) {
         controlType = _controlType;
@@ -36,14 +37,20 @@ public class UIController : MonoBehaviour {
     [Header("Updated Control Setting UI")]
     public bool UpdatedSettingMode = false;
     public GameObject[] playerUIObjects;
+    public Color[] playerColors;
     public Color inGameColor;
     public Color notInGameColor;
     public GameObject[] textPrompts;
 
+    private PlayerUIObject[] playerUI;
+    private bool[] selectedColors;
+
     private int nextPlayerToInit = 0;
     private List<ControlSetting> playerControls;
     private bool keyboardPlayerFound = false;
+    private int keyboardPlayerIndex = -1;
     private bool[] controllerIndexFound = { false, false, false, false };
+    private int[] controllerPlayerIndicies = { -1, -1, -1, -1 };
 
     [Header("Game UI")]
     public bool GameMode = false;
@@ -56,6 +63,29 @@ public class UIController : MonoBehaviour {
     private List<bool> isSet;
 
     private AudioController audioC;
+
+    private class PlayerUIObject {
+
+        public TextMeshProUGUI playerText;
+        public TextMeshProUGUI controlType;
+        public GameObject playerIn;
+        public GameObject playerNotIn;
+
+        public GameObject colorSelectedEdge;
+        public Animator colorSelectedEdgeAnim;
+        public Image colorSelectedImage;
+
+        public PlayerUIObject(Transform player) {
+            playerText = player.Find("PlayerText").GetComponent<TextMeshProUGUI>();
+            controlType = playerText.transform.Find("ControlType").GetComponent<TextMeshProUGUI>();
+            playerIn = player.Find("PlayerIn").gameObject;
+            playerNotIn = player.Find("PlayerNotIn").gameObject;
+
+            colorSelectedEdge = player.Find("ColorSelectedEdge").gameObject;
+            colorSelectedEdgeAnim = colorSelectedEdge.GetComponent<Animator>();
+            colorSelectedImage = colorSelectedEdge.transform.Find("ColorSelected").GetComponent<Image>();
+        }
+    }
 
     void Start() {
         audioC = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioController>();
@@ -72,6 +102,22 @@ public class UIController : MonoBehaviour {
                 new ControlSetting(ControlType.Controller, false),
                 new ControlSetting(ControlType.Controller, false)
             };
+
+            selectedColors = new bool[playerColors.Length];
+            for (int i = 0; i < selectedColors.Length; i++) {
+                selectedColors[i] = false;
+            }
+
+            playerUI = new PlayerUIObject[] {
+                new PlayerUIObject(playerUIObjects[0].transform),
+                new PlayerUIObject(playerUIObjects[1].transform),
+                new PlayerUIObject(playerUIObjects[2].transform),
+                new PlayerUIObject(playerUIObjects[3].transform)
+            };
+
+            for (int i = 0; i < playerUI.Length; i++) {
+                playerUI[i].colorSelectedImage.color = Color.white;
+            }
         }
 
         if (GameMode) {
@@ -95,15 +141,36 @@ public class UIController : MonoBehaviour {
                 }
             }
 
+            if (Input.GetKeyDown(KeyCode.X) && keyboardPlayerFound) {
+                playerUI[keyboardPlayerIndex].colorSelectedImage.color = GetNextFreeColor(playerUI[keyboardPlayerIndex].colorSelectedImage.color);
+
+            } else if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.Any)) {
+                if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.One) && controllerIndexFound[0]) {
+                    playerUI[controllerPlayerIndicies[0]].colorSelectedImage.color = GetNextFreeColor(playerUI[controllerPlayerIndicies[0]].colorSelectedImage.color);
+
+                } else if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.Two) && controllerIndexFound[1]) {
+                    playerUI[controllerPlayerIndicies[1]].colorSelectedImage.color = GetNextFreeColor(playerUI[controllerPlayerIndicies[1]].colorSelectedImage.color);
+
+                } else if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.Three) && controllerIndexFound[2]) {
+                    playerUI[controllerPlayerIndicies[2]].colorSelectedImage.color = GetNextFreeColor(playerUI[controllerPlayerIndicies[2]].colorSelectedImage.color);
+
+                } else if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.Four) && controllerIndexFound[3]) {
+                    playerUI[controllerPlayerIndicies[3]].colorSelectedImage.color = GetNextFreeColor(playerUI[controllerPlayerIndicies[3]].colorSelectedImage.color);
+                }
+            }
+
             if (nextPlayerToInit < 4) {
 
                 if (Input.GetKeyDown(KeyCode.X) && !keyboardPlayerFound) {
-                    Transform currentPlayer = playerUIObjects[nextPlayerToInit].transform;
-                    currentPlayer.Find("PlayerText").GetComponent<TextMeshProUGUI>().color = inGameColor;
-                    currentPlayer.Find("PlayerNotIn").gameObject.SetActive(false);
-                    currentPlayer.Find("PlayerIn").gameObject.SetActive(true);
-                    currentPlayer.Find("ControlType").GetComponent<TextMeshProUGUI>().text = "Keyboard";
-                    playerControls[nextPlayerToInit] = new ControlSetting(ControlType.Keyboard);
+                    playerUI[nextPlayerToInit].playerText.color = inGameColor; // set color to green (in game)
+                    playerUI[nextPlayerToInit].playerNotIn.SetActive(false); // disable crying image
+                    playerUI[nextPlayerToInit].playerIn.SetActive(true); // enable dancing image
+                    playerUI[nextPlayerToInit].colorSelectedEdgeAnim.SetTrigger("Waiting"); // set animation to prompt for color select
+                    playerUI[nextPlayerToInit].colorSelectedImage.color = GetNextFreeColor();
+                    playerUI[nextPlayerToInit].controlType.text = "Keyboard"; // set control type found
+                    playerControls[nextPlayerToInit] = new ControlSetting(ControlType.Keyboard); // update control type in backend
+
+                    keyboardPlayerIndex = nextPlayerToInit;
 
                     nextPlayerToInit++;
 
@@ -114,27 +181,32 @@ public class UIController : MonoBehaviour {
                     if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.One) && !controllerIndexFound[0]) {
                         newControl.controllerIndex = GamePad.Index.One;
                         controllerIndexFound[0] = true;
+                        controllerPlayerIndicies[0] = nextPlayerToInit;
 
                     } else if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.Two) && !controllerIndexFound[1]) {
                         newControl.controllerIndex = GamePad.Index.Two;
                         controllerIndexFound[1] = true;
+                        controllerPlayerIndicies[1] = nextPlayerToInit;
 
                     } else if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.Three) && !controllerIndexFound[2]) {
                         newControl.controllerIndex = GamePad.Index.Three;
                         controllerIndexFound[2] = true;
+                        controllerPlayerIndicies[2] = nextPlayerToInit;
 
                     } else if (GamePad.GetButtonDown(GamePad.Button.X, GamePad.Index.Four) && !controllerIndexFound[3]) {
                         newControl.controllerIndex = GamePad.Index.Four;
                         controllerIndexFound[3] = true;
+                        controllerPlayerIndicies[3] = nextPlayerToInit;
                     }
 
                     if (newControl.controllerIndex != GamePad.Index.Any) {
-                        Transform currentPlayer = playerUIObjects[nextPlayerToInit].transform;
-                        currentPlayer.Find("PlayerText").GetComponent<TextMeshProUGUI>().color = inGameColor;
-                        currentPlayer.Find("PlayerNotIn").gameObject.SetActive(false);
-                        currentPlayer.Find("PlayerIn").gameObject.SetActive(true);
-                        currentPlayer.Find("ControlType").GetComponent<TextMeshProUGUI>().text = "Controller";
-                        playerControls[nextPlayerToInit] = newControl;
+                        playerUI[nextPlayerToInit].playerText.color = inGameColor; // set color to green (in game)
+                        playerUI[nextPlayerToInit].playerNotIn.SetActive(false); // disable crying image
+                        playerUI[nextPlayerToInit].playerIn.SetActive(true); // enable dancing image
+                        playerUI[nextPlayerToInit].colorSelectedEdgeAnim.SetTrigger("Waiting"); // set animation to prompt for color select
+                        playerUI[nextPlayerToInit].colorSelectedImage.color = GetNextFreeColor();
+                        playerUI[nextPlayerToInit].controlType.text = "Keyboard"; // set control type found
+                        playerControls[nextPlayerToInit] = newControl; // update control type in backend
 
                         nextPlayerToInit++;
 
@@ -159,7 +231,47 @@ public class UIController : MonoBehaviour {
         }
     }
 
-	public void GoTo(string sceneName) {
+    private Color GetNextFreeColor() {
+        if (selectedColors.Length <= 0) {
+            return Color.black;
+        }
+        for (int i = 0; i < selectedColors.Length; i++) {
+            if (!selectedColors[i]) {
+                selectedColors[i] = true;
+                return playerColors[i];
+            }
+        }
+        return Color.black;
+    }
+
+    private Color GetNextFreeColor(Color afterThis) {
+        if (selectedColors.Length <= 0) {
+            return Color.black;
+        }
+        // find index of afterThis
+        int afterThisIndex = -1;
+        for (int i = 0; i < playerColors.Length; i++) {
+            if (playerColors[i] == afterThis) {
+                afterThisIndex = i;
+            }
+        }
+        if (afterThisIndex == -1) {
+            return Color.black;
+        }
+
+        // get the next useable color afterwards
+        for (int i = 0; i < selectedColors.Length; i++) {
+            int curIndex = (afterThisIndex + i) % selectedColors.Length;
+            if (!selectedColors[curIndex]) {
+                selectedColors[afterThisIndex] = false;
+                selectedColors[curIndex] = true;
+                return playerColors[curIndex];
+            }
+        }
+        return playerColors[afterThisIndex];
+    }
+
+    public void GoTo(string sceneName) {
         Time.timeScale = 1;
         SceneManager.LoadScene(sceneName);
     }
@@ -239,6 +351,10 @@ public class UIController : MonoBehaviour {
         if (UpdatedSettingMode & canContinue) {
             SettingManager.HasBeenSetUp = true;
             SettingManager.NumberOfPlayers = nextPlayerToInit;
+
+            for (int i = 0; i < SettingManager.NumberOfPlayers; i++) {
+                playerControls[i].selectedColor = playerUI[i].colorSelectedImage.color;
+            }
             SettingManager.PlayerControls = playerControls;
 
             PrintCurrentSettings();
@@ -277,7 +393,10 @@ public class UIController : MonoBehaviour {
 
     void PrintCurrentSettings() {
         for (int i = 0; i < nextPlayerToInit; i++) {
-            Debug.Log(string.Format("Player:{0}, Setting={1}", (i + 1).ToString(), playerControls[i].controllerIndex));
+            Debug.Log(string.Format("Player:{0}, Setting={1}", 
+                (i + 1).ToString(), 
+                (playerControls[i].controllerIndex != GamePad.Index.Any) ? "Controller " + playerControls[i].controllerIndex.ToString() : "Keyboard")
+            );
         }
         /*
         for (int i = 0; i < numPlayers; i++) {
